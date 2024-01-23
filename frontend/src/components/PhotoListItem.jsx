ec30c226264dbde2f19fc76a6c48353870b1431e
svaronc <sebastianvaron96@gmail.com>
2024-01-23 17:30:03 -0500

    styles for photoItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,21 +2,26 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const PhotoListItem = ({sampleData}) => {
+const PhotoListItem = ({ sampleData }) => {
   const { username, id, imageSource, location, profile } = sampleData;
-  const locationString = `${location.city} ${location.country}`;
+  const locationString = `${location.city}, ${location.country}`;
 
   return (
-    <div key={id}>
-      <img src={imageSource} alt="picture"/>
-      <img src={profile} alt="profile image" />
-      <h4>{username}</h4>
-      <p>{locationString}</p>
+    <div className="photo-list__item" key={id}>
+      <img className="photo-list__image" src={imageSource} alt="picture" />
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={profile}
+          alt="profile image"
+        />
+        <h4 className="photo-list__user-info">
+          {username} <br />{" "}
+          <span className="photo-list__user-location"> {locationString}</span>
+        </h4>
+      </div>
     </div>
-  )
+  );
 };
-
-
 
 export default PhotoListItem;
