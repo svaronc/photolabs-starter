a76854418eaea51e026f83c13e3008c498af0de7
svaronc <sebastianvaron96@gmail.com>
2024-01-25 22:38:21 -0500

    implement useReducer to handle de state

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,14 +3,30 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-const PhotoListItem = ({ sampleData , favorites, updateToFavPhotoIds,setPhotoSelected}) => {
-  const { user, id, urls, location ,similar_photos} = sampleData;
+const PhotoListItem = ({
+  sampleData,
+  favorites,
+  updateToFavPhotoIds,
+  setPhotoSelected,
+}) => {
+  const { user, id, urls, location, similar_photos } = sampleData;
   const locationString = `${location.city}, ${location.country}`;
-  
+
   return (
     <div className="photo-list__item" key={id}>
-      <PhotoFavButton favorites = {favorites} updateToFavPhotoIds = {updateToFavPhotoIds} id = {id}/>
-      <img onClick={() => setPhotoSelected(id,user,urls,locationString,similar_photos)} className="photo-list__image" src={urls.regular} alt="picture" />
+      <PhotoFavButton
+        favorites={favorites}
+        updateToFavPhotoIds={updateToFavPhotoIds}
+        id={id}
+      />
+      <img
+        onClick={() => {
+          setPhotoSelected(id, user, urls, locationString, similar_photos);
+        }}
+        className="photo-list__image"
+        src={urls.regular}
+        alt="picture"
+      />
       <div className="photo-list__user-details">
         <img
           className="photo-list__user-profile"
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,39 +1,77 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+
+export const ACTIONS = {
+  TOGGLE_FAVORITE: 'TOGGLE_FAVORITE',
+  SET_PHOTO_SELECTED: 'SET_PHOTO_SELECTED',
+  CLOSE_PHOTO_DETAILS_MODAL: 'CLOSE_PHOTO_DETAILS_MODAL'
+};
+
+const initialState = {
+  visible: false,
+  photoDetails: {},
+  favorites: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "TOGGLE_FAVORITE":
+      const isFavorite = state.favorites.includes(action.id);
+      return {
+        ...state,
+        favorites: isFavorite
+          ? state.favorites.filter((photoId) => photoId !== action.id)
+          : [...state.favorites, action.id],
+      };
+
+    case "SET_PHOTO_SELECTED":
+      return {
+        ...state,
+        visible: !state.visible,
+        photoDetails: {
+          id: action.id,
+          username: action.user.username,
+          url: action.urls.full,
+          profile: action.user.profile,
+          locationString: action.locationString,
+          similar_photos: action.similar_photos,
+        },
+      };
+
+    case "CLOSE_PHOTO_DETAILS_MODAL":
+      return {
+        ...state,
+        visible: false,
+        photoDetails: {},
+      };
+
+    default:
+      return state;
+  }
+}
 
 export const useApplicationData = () => {
-  const [visible, setVisible] = useState(false);
-  const [photoDetails, setPhotoDetails] = useState({});
-  const [favorites, setFavorites] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return {
-    state: {
-      favorites,
-      visible,
-      photoDetails,
-    },
+    state,
     updateToFavPhotoIds: (id) => {
-      const isFavorite = favorites.includes(id);
-      if(isFavorite) {
-        setFavorites(favorites.filter(photoId => photoId !== id))
-      } else {
-        setFavorites([...favorites, id])
-      }
-      
+      console.log(state);
+      dispatch({ type: "TOGGLE_FAVORITE", id });
     },
-    setPhotoSelected: (id,user,urls,locationString,similar_photos) => {
-      setVisible((prev) => !prev);
-      setPhotoDetails({
+    setPhotoSelected: (id, user, urls, locationString, similar_photos) => {
+      console.log(state);
+      dispatch({
+        type: "SET_PHOTO_SELECTED",
         id,
-        username: user.username,
-        url: urls.full,
-        profile: user.profile,
-        locationString: locationString,
-        similar_photos: similar_photos,
+        user,
+        urls,
+        locationString,
+        similar_photos,
       });
     },
     onClosePhotosDetailsModal: () => {
-      setVisible(false);
-      setPhotoDetails({});
+      console.log(state);
+      dispatch({ type: "CLOSE_PHOTO_DETAILS_MODAL" });
     },
   };
 };
